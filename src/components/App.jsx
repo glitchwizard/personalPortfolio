fdb28482e2648b523ee6cdb8c9da40c8fcce6006
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-15 10:05:03 -0800

    update page color scheme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,26 +13,10 @@ function App() {
       margin: 0px;
       padding: 0px;
       }
-
-      body {
-          margin: 0px;
-          padding: 0px;
-      }
-
-      div {
-          margin: 100px;
-          padding: 0;
-      }
-
-      p {
-          color: red;
-      }
     `}
       </style>
       <div>
-        <p>
               test DIV
-        </p> 
       </div>
     THIS IS A TEST FONT.
     </div>
