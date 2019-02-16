2abdef5c8622b2de13c08002a3469f878b291e10
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-15 17:05:56 -0800

    adjust image sizing

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,38 +11,59 @@ function About() {
                     padding-top: 15px;
                     padding-bottom: 15px;
                     display: block;
+                    min-height: 600px;
                 }
 
                 .imgContainerWrapper {
                     position: relative;
-                    border: 1px solid green;
+                    border: 5px solid blue;
                     display: block;
                     padding: 0px;
                     margin: 0px;
-                    min-height: 295px;
+                    min-height: 265px;
+                    max-height: 510px;
                 }
 
                 .imgContainer {
-                    border: 1px solid blue;
-                    margin: 0px;
+                    border: 1px solid red;
+                    margin: 10px;
                     padding: 0px;
                     position: absolute;
                     overflow: hidden;
-                    max-width: 500px;
-                    max-height: 500px;
+                    width: 40%;
+                    height: 50vh;
+                    max-width: 475px;
+                    max-height: 475px;
                     min-height: 250px;
                     min-width: 250px;
                     right: 0px;
                     display: inline-block;
+                    text-align: center;
+                }
+
+                .imgWall {
+                    text-align: center; 
+                    position: absolute; 
+                    top: 50%; 
+                    left: 50%; 
+                    transform: translate(-50%, -40%);
+                    padding-top: 7%;
+                }
+
+                img {
+                    margin: auto;
+                    display: inline-block;
+                    height: 10%;
                 }
             `}
       </style>
         <div className="imgContainerWrapper">   
-            OUTSIDE SECOND BLOCK
-            <div className="imgContainer">
-            INSIDE SECOND BLOCK
-            </div>
-      </div>
+                <div className="imgContainer">
+                    <div className="imgWall">
+                        <img src="https://charlesvictus.files.wordpress.com/2016/12/10985961_10101827394927908_5039593149398642900_n.jpg" />
+                    </div>
+                </div>
+        </div>
     </div>
   );
 }
