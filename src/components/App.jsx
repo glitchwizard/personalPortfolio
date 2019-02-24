d7d7f8ffb5a45fbc656cab2bfa3439efc119efd0
charley mcgowan <charley.mcgowan@outlook.com>
2019-02-23 17:20:11 -0800

    add Particles to the page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Splash from './Splash/Splash';
 import Portfolio from './Portfolio/Portfolio';
 import About from './About/About';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
@@ -9,14 +10,41 @@ function App() {
       <style jsx>{`
         .mainPage {
           font-family: "Montserrat", sans-serif;
-          
+          position: relative;
         }
       @import url("https://fonts.googleapis.com/css?family=Montserrat");
+
+      .pageItems {
+        position: absolute;
+        top: 0px;
+      }
+
+      .particleWrapper {
+
+      }
+
+      .particleCanvas {
+
+      }
       `}
       </style>
-      <Splash />
-      <Portfolio />
-      <About />
+        <Particles 
+          className="particleWrapper"
+          canvasClassName="particleCanvas"
+          height="100vh" 
+          width="100vw"
+          style={{
+            zIndex: "-1",
+            position: "fixed",
+            top: '0px',
+            margin: '0',
+            padding: '0'
+          }}/> 
+      <div className='pageItems'>
+        <Splash />
+        <Portfolio />
+        <About />
+      </div>
     </div>
   );
 }
