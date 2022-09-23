939486e214ef802a44f949771f17c9ae28f8cb3d
Charley McGowan <glitchwizard@gmail.com>
2022-09-23 01:14:31 -0400

    updated with more modern code

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import { AppContainer } from 'react-hot-loader';
 
-const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('react-app-root')
-  );
-};
-
-render(App);
-/*eslint-disable */
-if (module.hot) {
-    module.hot.accept('./components/App', () => {
-        render(App);
-    });
-}
-/*eslint-enable */
+const root = ReactDOM.createRoot(document.getElementById('react-app-root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
