3693e9c870e1173e06f149c644fca1ac98cd71aa
Charley McGowan <glitchwizard@gmail.com>
2022-09-26 21:46:49 -0400

    Fixed all the little issues

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,52 +1,14 @@
 import React from 'react';
 import AboutBioBox from './AboutBioBox';
+import styles from './About.module.css';
 
 function About() {
   return (
-    <div className="componentStyle">
-      <style jsx>{`
-                .componentStyle {
-                    border: 1px solid white;
-                    margin: 8px;
-                    margin-top: 25px;
-                    padding-top: 15px;
-                    padding-bottom: 15px;
-                    display: flex;
-                    min-height: 500px;
-                    flex-grow: 1;
-                }
-
-                .imgContainerWrapper {
-                    width: 50%;
-                    position: relative;
-                    overflow: hidden;
-                    border: 1px solid white;
-                    margin: 10px;
-                }
-
-                .imgContainer {
-                    height: 100%;
-                    left: 50%;
-                    position: absolute;
-                    overflow: hidden;
-                    transform: translate(-50%, 0%);
-
-                }
-
-                .imgWall {
-                }
-
-                .aboutImg {
-                  min-height: 400px;
-                  object-fit: cover;
-                }
-            `}
-      </style>
-      
+    <div className={styles.componentStyle}>      
       <AboutBioBox />
-      <div className="imgContainerWrapper">   
-        <div className="imgContainer">
-          <img className="aboutImg" src="https://charlesvictus.files.wordpress.com/2016/12/10985961_10101827394927908_5039593149398642900_n.jpg" />
+      <div className={styles.imgContainerWrapper}>   
+        <div className={styles.imgContainer}>
+          <img className={styles.aboutImg} src="https://charlesvictus.files.wordpress.com/2016/12/10985961_10101827394927908_5039593149398642900_n.jpg" />
         </div>
       </div>
     </div>
--- src/components/About/AboutBioBox.jsx
+++ src/components/About/AboutBioBox.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
+import styles from './AboutBioBox.module.css';
 
 function AboutBioBox() {
   return (
-    <div className="componentStyle">
-      <style jsx>{`
-        .componentStyle {
-        border: 1px solid white;
-        margin: 25px;
-        padding: 15px;
-        width: calc(50%);
-        background-color: rgba(0,0,0,0.5);
-        min-width: 180px;
-        }
-        
-        h1 {
-          text-align: center;
-        }
-        .mainBioContainer {
-
-        }
-            `}
-      </style>
-      <div className="mainBioContainer">
+    <div className={styles.componentStyle}>
+      <div className={styles.mainBioContainer}>
         <h1>ABOUT</h1>
         <hr />
         <ul>
