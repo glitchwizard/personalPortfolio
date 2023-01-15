c7f90bd77db7b058ab763175d2daffa3e12b36ce
Charley McGowan <glitchwizard@gmail.com>
2023-01-14 17:27:47 -0800

    Improved performance of the whole site

--- src/components/Coding/Coding.jsx
+++ src/components/Coding/Coding.jsx
@@ -1,36 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Coding.module.css';
+import {GitHub} from '@mui/icons-material';
 
-const Coding = ({cssClass, gitHubRepos, gitHubUser}) => {
+const Coding = (
+  {cssClass, 
+    gitHubRepos, 
+    gitHubUser,
+    areGitHubReposLoaded,
+    isGitHubUserLoaded
+  }) => {
   
   return (
     <div className={`${cssClass} ${styles.coding}`}>
       <h2> CODING </h2> <hr />
-      GitHub:
-      <a href={gitHubUser.html_url}>
-        <img
-          className={styles.githubAvatar}
-          src={gitHubUser.avatar_url}
-          alt=""
-        />
-        {gitHubUser.login}
-      </a>
-      <h3> Most Recently Updated Repos: </h3>
-      (This list is Updated in nearly real time) <p />
-      <div className={styles.repoList}>
-        {gitHubRepos.map((repo, index) => (
-          <a href={repo.html_url} className={styles.repoName} key={repo.name}>
-            <div className={styles.repoListItem}>
-              <h3 className={styles.noWrap}>{index + 1} - {repo.name}</h3>
-              <hr />
-              <div className={styles.repoInnerCard}>
-                <p className={styles.repodDesc}> {repo.description} </p>
-              </div>
-            </div>
+      {isGitHubUserLoaded ?
+        <div>
+          <GitHub /> GitHub:
+          <a href={gitHubUser.html_url}>
+            <img
+              className={styles.githubAvatar}
+              src={gitHubUser.avatar_url}
+              alt=""
+            />
+            {gitHubUser.login}
           </a>
-        ))}
-      </div>
+        </div>
+        : <div>
+          <GitHub /> Loading...      
+        </div> }
+      {areGitHubReposLoaded ?
+        <div>
+          <h3> Most Recently Updated Repos: </h3>
+          (This list is Updated in nearly real time) <p />
+          <div className={styles.repoList}>
+            {gitHubRepos.map((repo, index) => (
+              <a href={repo.html_url} className={styles.repoName} key={repo.name}>
+                <div className={styles.repoListItem}>
+                  <h3 className={styles.noWrap}>{index + 1} - {repo.name}</h3>
+                  <hr />
+                  <div className={styles.repoInnerCard}>
+                    <p className={styles.repodDesc}> {repo.description} </p>
+                  </div>
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
+        : <div> Loading... </div>
+      }
     </div>
   );
 };
@@ -38,7 +56,9 @@ const Coding = ({cssClass, gitHubRepos, gitHubUser}) => {
 Coding.propTypes = {
   cssClass: PropTypes.object,
   gitHubRepos: PropTypes.object,
-  gitHubUser: PropTypes.object
+  gitHubUser: PropTypes.object,
+  areGitHubReposLoaded: PropTypes.bool,
+  isGitHubUserLoaded: PropTypes.bool
 };
 
 Coding.defaultProps = {};
