37b46ffa03f2627110181ade183e155584eb9b2d
charley mcgowan <charley.mcgowan@outlook.com>
2023-03-06 17:14:50 -0800

    IntroBlock finished

--- src/components/Splash/IntroBlock.jsx
+++ src/components/Splash/IntroBlock.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
 import styles from './IntroBlock.module.css';
 import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import {Container} from '@mui/material';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: 'rgba(0, 0, 0, 0.1)',
-  ...theme.typography.body2,
-  padding: theme.spacing(0.8),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
-const stackSpacing = 1;
-const stackMargin = 1;
-const stackDirection = { xs: 'column', md: 'row' };
 
 function IntroBlock() {
   return (
@@ -29,56 +14,54 @@ function IntroBlock() {
       <Divider my={10}>
         McGOWAN
       </Divider>
-      <Typography variant='body2'>
-        <Grid container
-          spacing={0.5}
-          justifyContent='center'
-          ml={-2}
+      <Grid container
+        justifyContent='center'
+        id='mainBlock'
+        rowSpacing={3}
+      >
+        <Grid item xs={12}
+          id='blockRow1'
         >
-          <Grid item xs={10}>
-            <Container>
-              <Stack 
-                direction={stackDirection}
-                divider={<Divider orientation='vertical' flexItem />}
-                spacing={stackSpacing}
-                id='IntroBlockStack1'
-                margin={stackMargin}
-              >
-                <Item>
-                  ENGINEER
-                </Item>
-                <Item>
-                  DEVELOPER
-                </Item>
-                <Item>
-                  ARTIST
-                </Item>
-              </Stack>
-            </Container>
+          <Grid container
+            justifyContent={'center'}
+            id='blockRow1Container'
+            spacing={0.5}
+          >
+            {
+              ['ENGINEER', 'DEVELOPER', 'ARTIST'].map((skill) => (
+                <Grid key={skill} item>
+                  <Paper
+                    className={styles.itemStyle}
+                    sx={{p:0.5, m:0.5}}
+                  >
+                    {skill}
+                  </Paper>
+                </Grid>
+              ))
+            }
           </Grid>
-          <Grid item xs={10}>
-            <div >
-              <Stack 
-                direction={stackDirection}
-                divider={<Divider orientation='vertical' flexItem />}
-                spacing={stackSpacing}
-                id='IntroBlockStack2'
-                margin={stackMargin}
-              >
-                <Item>
-                  CONSULTANT
-                </Item>
-                <Item>
-                  ENTERTAINER
-                </Item>
-                <Item>
-                  BUILDER
-                </Item>
-              </Stack>
-            </div>
+          <Grid item xs={12}>
+            <Grid container 
+              justifyContent={'center'}
+              spacing={0.5}
+              id='blockRow2Container'
+            >
+              {
+                ['CONSULTANT', 'ENTERTAINER', 'BUILDER'].map((skill) => (
+                  <Grid key={skill} item>
+                    <Paper 
+                      className={styles.itemStyle}
+                      sx={{p:0.5}}
+                    >
+                      {skill}
+                    </Paper>
+                  </Grid>
+                ))
+              }
+            </Grid>
           </Grid>
         </Grid>
-      </Typography>
+      </Grid>
     </Paper>
   );
 }
